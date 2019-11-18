7f186264279a0930a287c834ecc3ca81f9333800
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2019-11-18 15:55:00 +0100

    throttling

--- script_template.js
+++ script_template.js
@@ -1,16 +1,16 @@
-var s = {} // Settings
+var settings = {}
 
-/// EDIT SETTINGS BELOW
-s.apiKey = '%%APIKEY%%'
-s.threshold_demographics = 0.5 // range [0, 1] - Applies to the DEMOGRAPHICS API
-s.threshold_general = 0.9 // range [0, 1] - Applies to the GENERAL API
+/// YOU CAN EDIT SETTINGS BELOW
+settings.apiKey = '%%APIKEY%%'
+settings.threshold_demographics = 0.5 // range [0, 1] - Applies to the DEMOGRAPHICS API
+settings.threshold_general = 0.9 // range [0, 1] - Applies to the GENERAL API
 /// END OF SETTINGS
 
 
 /// MACHINERY
 
 // Download the CSV from the DMI tool at https://tools.digitalmethods.net/beta/googleImages/
-var app
+var app, queue
 artoo.scrapeTable('table', {
   headers: 'th',
   done: function(table){
@@ -20,22 +20,99 @@ artoo.scrapeTable('table', {
 		artoo.injectScript('https://sdk.clarifai.com/js/clarifai-latest.js', function() {
 			// Connect to the API via API key
 			app = new Clarifai.App({
-				apiKey: s.apiKey
+				apiKey: settings.apiKey
 			})
 			console.log('...API loaded')
 
-			var urls = table.map(function(d){return d['thumbnail url']})
-				// .filter(function(d,i){return i<10})
+			queue = newQueue(
+				table,
+				table.map(function(d){return d['thumbnail url']})
+					// .filter(function(d,i){return i<30})
+			)
+			queue.nextBatch()
+		})
+  }
+})
 
-			if (urls.length>100) {
-				alert("/!\\ You cannot query more than 100 URLS at once; please use different s. The process has been stopped.")
-				return
+function newQueue(table, urls) {
+	var batchSize = 100
+	var tableIndex = {}
+	table.forEach(function(row,i){
+		tableIndex[row['thumbnail url']] = row
+	})
+	return {
+		urls: urls || [],
+		tableIndex: tableIndex,
+		nextBatch: function() {
+			if (this.urls.length == 0) {
+				// Save the CSV
+		  	console.log("Download CSV")
+		  	artoo.saveCsv(Object.values(this.tableIndex))
+			} else {
+				var i = batchSize
+				var batch = []
+				while(i-->0 && this.urls.length>0) {
+					batch.push(this.urls.pop())
+				}
+				runClarifaiBatch(batch)
 			}
+		},
+		updateRow: function(url, o) {
+			var k,v
+			for (k in o) {
+				this.tableIndex[url][k] = o[k]
+			}
+		}
+	}
+}
 
-			// Query DEMOGRAPHICS API
-			console.log('Querying Clarifai "Demographics" API to enrich data...')
+function runClarifaiBatch(urls) {
+	// Query DEMOGRAPHICS API
+	console.log('Querying Clarifai "Demographics" API to enrich data...')
+	app.models.predict(
+    'c0c0ac362b03416da06ab3fa36fb58e3', // Demographics API
+    urls)
+  .then(function(response){
+  	console.log('...Clarifai responded.')
+  	if (!response.outputs || response.outputs.length == 0) {
+  		console.error("Clarifai response has no outputs")
+  	} else {
+	    console.log('Response has ' + response.outputs.length + ' outputs')
+
+	    // Enrich the CSV with the response.
+	    urls.forEach(function(url, i){
+	    	var row={}
+	    	if (!response.outputs[i] || !response.outputs[i].data) {
+	    		// console.log('  - Output ' + i + ' has no data')
+	    	} else {
+		    	var data = response.outputs[i].data
+		    	if (data && data.regions) {
+		    		data.regions.forEach(function(region){
+		    			var face = region.data.face
+		    			if (face && face.gender_appearance) {
+		    				face.gender_appearance.concepts.forEach(function(concept){
+		    					if (concept.value >= settings.threshold_demographics) {
+		    						row[concept.name] = (row[concept.name] || 0) + 1
+		    					}
+		    				})
+		    			}
+		    			if (face && face.multicultural_appearance) {
+		    				face.multicultural_appearance.concepts.forEach(function(concept){
+		    					if (concept.value >= settings.threshold_demographics) {
+		    						row[concept.name] = (row[concept.name] || 0) + 1
+		    					}
+		    				})
+		    			}
+		    		})
+		    	}
+	    	}
+	    	queue.updateRow(url, row)
+	    })
+	    
+	    // Query GENERAL API
+			console.log('Querying Clarifai "General" API to enrich data...')
 			app.models.predict(
-		    'c0c0ac362b03416da06ab3fa36fb58e3', // Demographics API
+		    'aaa03c23b3724a16a56b629203edc62c', // General API
 		    urls)
 		  .then(function(response){
 		  	console.log('...Clarifai responded.')
@@ -45,83 +122,30 @@ artoo.scrapeTable('table', {
 			    console.log('Response has ' + response.outputs.length + ' outputs')
 
 			    // Enrich the CSV with the response.
-			    table.forEach(function(row, i){
+			    urls.forEach(function(url, i){
+			    	var row = {}
 			    	if (!response.outputs[i] || !response.outputs[i].data) {
-			    		console.log('  - Output ' + i + ' has no data')
+			    		// console.log('  - Output ' + i + ' has no data')
 			    	} else {
 				    	var data = response.outputs[i].data
-				    	if (data && data.regions) {
-				    		data.regions.forEach(function(region){
-				    			var face = region.data.face
-				    			if (face && face.gender_appearance) {
-				    				face.gender_appearance.concepts.forEach(function(concept){
-				    					if (concept.value >= s.threshold_demographics) {
-				    						row[concept.name] = (row[concept.name] || 0) + 1
-				    					}
-				    				})
-				    			}
-				    			if (face && face.multicultural_appearance) {
-				    				face.multicultural_appearance.concepts.forEach(function(concept){
-				    					if (concept.value >= s.threshold_demographics) {
-				    						row[concept.name] = (row[concept.name] || 0) + 1
-				    					}
-				    				})
-				    			}
-				    		})
+			    		// console.log(' - Output data', data)
+				    	if (data && data.concepts) {
+				    		row["general concepts"] = data.concepts
+				    			.filter(function(c){return c.value >= settings.threshold_general})
+				    			.map(function(c){return c.name})
+				    			.join(", ")
 				    	}
 			    	}
+			    	queue.updateRow(url, row)
 			    })
 			    
-			    // Query GENERAL API
-					console.log('Querying Clarifai "General" API to enrich data...')
-					app.models.predict(
-				    'aaa03c23b3724a16a56b629203edc62c', // General API
-				    urls)
-				  .then(function(response){
-				  	console.log('...Clarifai responded.')
-				  	if (!response.outputs || response.outputs.length == 0) {
-				  		console.error("Clarifai response has no outputs")
-				  	} else {
-					    console.log('Response has ' + response.outputs.length + ' outputs')
-
-					    // Enrich the CSV with the response.
-					    table.forEach(function(row, i){
-					    	if (!response.outputs[i] || !response.outputs[i].data) {
-					    		console.log('  - Output ' + i + ' has no data')
-					    	} else {
-						    	var data = response.outputs[i].data
-					    		// console.log(' - Output data', data)
-						    	if (data && data.concepts) {
-						    		row["general concepts"] = data.concepts
-						    			.filter(function(c){return c.value >= s.threshold_general})
-						    			.map(function(c){return c.name})
-						    			.join(", ")
-						    	}
-					    	}
-					    })
-					    
-					  	// Save the CSV
-					  	console.log("Download CSV")
-					  	artoo.saveCsv(table)
-				  	}
-
-				  })
-				  .catch(function(error){
-				  	console.log('Something went wrong with the GENERAL api call')
-				  	alert('Something went wrong with the GENERAL api call (see console for details)')
-				    console.log('Error status code: ' + error.data['status']['code']);
-				    console.log('Error description: ' + error.data['status']['description']);
-				    if (error.data['status']['details'])
-				    {
-				      console.log('Error details: ' + error.data['status']['details']);
-				    }
-				  })
+			  	queue.nextBatch()
 		  	}
 
 		  })
 		  .catch(function(error){
-		  	console.log('Something went wrong with the DEMOGRAPHICS api call')
-		  	alert('Something went wrong with the DEMOGRAPHICS api call (see console for details)')
+		  	console.log('Something went wrong with the GENERAL api call')
+		  	alert('Something went wrong with the GENERAL api call (see console for details)')
 		    console.log('Error status code: ' + error.data['status']['code']);
 		    console.log('Error description: ' + error.data['status']['description']);
 		    if (error.data['status']['details'])
@@ -129,6 +153,17 @@ artoo.scrapeTable('table', {
 		      console.log('Error details: ' + error.data['status']['details']);
 		    }
 		  })
-		})
-  }
-})
+  	}
+
+  })
+  .catch(function(error){
+  	console.log('Something went wrong with the DEMOGRAPHICS api call')
+  	alert('Something went wrong with the DEMOGRAPHICS api call (see console for details)')
+    console.log('Error status code: ' + error.data['status']['code']);
+    console.log('Error description: ' + error.data['status']['description']);
+    if (error.data['status']['details'])
+    {
+      console.log('Error details: ' + error.data['status']['details']);
+    }
+  })
+}
